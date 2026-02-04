8b8c3a7ae0be05fd143427ed5898ba8b49d2cdc1
TRedBr <colin-brandes@gmx.de>
2026-02-04 14:28:05 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from print import *
+
+
+
+## controle block
+# x = input("What will it be called")
+# add_html("test")
+x= read_from_html("test")
+print(x)
---
+++ print.py
@@ -0,0 +1,26 @@
+def add_html(title):
+    with open(f"test.html", "at") as f:
+        f.write(f'<!DOCTYPE html>\n<html lang="en">\n<head>\n    <title>{title}</title>\n</head>\n<body>\n<h1>This is {title}</h1>\n</body>\n</html>')
+
+def rewrite_html(title):
+    with open(f"test.html", "wt") as f:
+        f.write(f'<!DOCTYPE html>\n<html lang="en">\n<head>\n    <title>{title}</title>\n</head>\n<body>\n<h1>This is {title}</h1>\n</body>\n</html>')
+
+def read_from_html(title):
+    red_html= ""
+    with open(f"{title}.html", "rt") as f:
+        for line in f:
+            if line.startswith("</body>"):
+                pass
+            elif line.startswith("</html>"):
+                pass
+            else:
+                red_html += f'{line}'
+    return red_html
+
+def add_paragraph_with_header_to_html(title):   #needs test if html exists
+    red_html = read_from_html(title)
+    headline = input("please enter a headline for the paragraph")
+    paragraph = input("please enter the paragraph")
+    with open(f"test.html", "at") as f:
+        f.write(f'\n<h2>{headline}</h2>\n<p>{paragraph}</p>\n</body>\n</html>')
